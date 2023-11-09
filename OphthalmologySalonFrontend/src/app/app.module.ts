9bc55f9d5c3d88773b169aa1a218c2e50a909577
soleshhaker <66915452+soleshhaker@users.noreply.github.com>
2023-11-09 15:03:15 +0100

    restructure, move app module and component to src/app

--- OphthalmologySalonFrontend/src/app/app.module.ts
+++ OphthalmologySalonFrontend/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { FetchVisitsComponent } from './fetch-visits/fetch-visits.component';
+import { HomeComponent } from './modules/home/home.component';
+import { NavMenuComponent } from './modules/nav-menu/nav-menu.component';
+import { FetchVisitsComponent } from './modules/fetch-visits/fetch-visits.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ErrorHandlerService } from 'src/app/core/services/error-handler.service';
 import { JwtModule } from "@auth0/angular-jwt";
-import { AuthGuard } from '../core/guards/auth.guard';
-import { ForbiddenComponent } from './forbidden/forbidden.component';
-import { AdminGuard } from '../core/guards/admin.guard';
+import { AuthGuard } from './core/guards/auth.guard';
+import { ForbiddenComponent } from './modules/forbidden/forbidden.component';
+import { AdminGuard } from './core/guards/admin.guard';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
 
@@ -35,8 +35,8 @@ export function tokenGetter() {
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'fetch-visits', component: FetchVisitsComponent, canActivate: [AuthGuard, AdminGuard] },
-      { path: 'authentication', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule) },
-      { path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
+      { path: 'authentication', loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule) },
+      { path: 'customer', loadChildren: () => import('./modules/customer/customer.module').then(m => m.CustomerModule) },
       { path: "forbidden", component: ForbiddenComponent },
     ]),
     JwtModule.forRoot({
--- OphthalmologySalonFrontend/src/main.ts
+++ OphthalmologySalonFrontend/src/main.ts
@@ -1,5 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/modules/app.module';
+import { AppModule } from './app/app.module';
 
 export function getBaseUrl() {
   return document.getElementsByTagName('base')[0].href;
